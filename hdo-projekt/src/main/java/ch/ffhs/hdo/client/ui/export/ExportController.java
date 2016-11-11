b3b427adb4a853a9d0cd81cea0ec091ea588b4a8
segjo <jonas_segessemann@hotmail.com>
2016-11-11 20:20:25 +0100

    Rename variables, set Path to Textfield

--- hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/export/ExportController.java
+++ hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/export/ExportController.java
@@ -1,5 +1,7 @@
 package ch.ffhs.hdo.client.ui.export;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ResourceBundle;
 
 import ch.ffhs.hdo.client.ui.base.Controller;
@@ -24,9 +26,17 @@ public class ExportController extends Controller<ExportModel, ExportView> {
 	public void initializeView() {
 		getView().setResourceBundle(getResourceBundle());
 		getView().setModel(getModel());
+		getView().addExportButtonListener(new ExportButtonListener());
 	}
 
-	
+
+	class ExportButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+			//TODO export configuration
+        	System.out.println("Export to: "+getView().getSelectedPath());
+		}
+    }
 	
 	
 }
--- hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/export/ExportModel.java
+++ hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/export/ExportModel.java
@@ -4,18 +4,5 @@ import ch.ffhs.hdo.client.ui.base.Model;
 
 public class ExportModel extends Model {
 
-	private String targetPath;
-	
-	public String getTargetPath() {
-		return targetPath;
-	}
-
-	public void setTargetPath(String targetPath) {
-
-		String oldValue = this.targetPath;
-		this.targetPath = targetPath;
-		firePropertyChange("targetPath", oldValue, targetPath);
-
-	}
 
 }
