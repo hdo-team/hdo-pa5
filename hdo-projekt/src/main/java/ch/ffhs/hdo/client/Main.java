b4aee791b41ff2686a21c32ace60d105377e0cfa
segjo <jonas_segessemann@hotmail.com>
2016-11-15 18:58:40 +0100

    Add Main Controller

--- hdo-projekt/src/main/java/ch/ffhs/hdo/client/Main.java
+++ hdo-projekt/src/main/java/ch/ffhs/hdo/client/Main.java
@@ -1,24 +1,25 @@
 package ch.ffhs.hdo.client;
 
-import javax.swing.JFrame;
-
 import ch.ffhs.hdo.client.ui.einstellungen.OptionController;
 import ch.ffhs.hdo.client.ui.einstellungen.OptionModel;
 import ch.ffhs.hdo.client.ui.export.ExportController;
 import ch.ffhs.hdo.client.ui.export.ExportModel;
+import ch.ffhs.hdo.client.ui.hauptfenster.MainController;
+import ch.ffhs.hdo.client.ui.hauptfenster.MainModel;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		// Init Controller
+		MainController mainController = new MainController(new MainModel());
 		OptionController optionController = new OptionController(new OptionModel());
 		ExportController exportController = new ExportController(new ExportModel());
 		// Start MainView
 
+		mainController.show();
 		optionController.show();
 		exportController.show();
-
 	
 
 	}
