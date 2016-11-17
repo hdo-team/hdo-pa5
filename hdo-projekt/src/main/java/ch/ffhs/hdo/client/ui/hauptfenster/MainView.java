06fb0ed999b7eeb52463c759440cbd4d34e51ca6
adpe <p.adrian@gmx.ch>
2016-11-17 22:46:18 +0100

    Importdialog Fehler
    Bei Klick auf Konfigurationen wurde ebenfalls dieses Fenster geöffnet.

--- hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/hauptfenster/MainView.java
+++ hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/hauptfenster/MainView.java
@@ -83,7 +83,6 @@ public class MainView extends View<MainModel> {
 
 			public void actionPerformed(ActionEvent e) {
 				getHandler().performOperation(OptionViewStartOperation.class);
-				getHandler().performOperation(ImportViewStartOperation.class);
 			}
 		});
 		
