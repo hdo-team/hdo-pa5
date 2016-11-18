b823baca1ab0ddbb5f3a1db4f5beb29206a4fd13
denisbittante <denisbittante@gmail.com>
2016-11-18 03:43:29 +0100

    Clean code

--- hdo-projekt/src/main/java/ch/ffhs/hdo/client/Main.java
+++ hdo-projekt/src/main/java/ch/ffhs/hdo/client/Main.java
@@ -1,15 +1,7 @@
 package ch.ffhs.hdo.client;
 
-import ch.ffhs.hdo.client.ui.einstellungen.OptionController;
-import ch.ffhs.hdo.client.ui.einstellungen.OptionModel;
-import ch.ffhs.hdo.client.ui.export.ExportController;
-import ch.ffhs.hdo.client.ui.export.ExportModel;
 import ch.ffhs.hdo.client.ui.hauptfenster.MainController;
 import ch.ffhs.hdo.client.ui.hauptfenster.MainModel;
-import ch.ffhs.hdo.client.ui.imports.ImportController;
-import ch.ffhs.hdo.client.ui.imports.ImportModel;
-import ch.ffhs.hdo.client.ui.regelset.RegelsetController;
-import ch.ffhs.hdo.client.ui.regelset.RegelsetModel;
 import ch.ffhs.hdo.infrastructure.ApplicationSettings;
 import ch.ffhs.hdo.persistence.jdbc.InitDatabase;
 
@@ -23,23 +15,11 @@ public class Main {
 
 		// Init Controller
 		MainController mainController = new MainController(new MainModel());
-		ExportController exportController = new ExportController(new ExportModel());
-		RegelsetController regelsetController = new RegelsetController(new RegelsetModel());
-		//ExportController exportController = new ExportController(new ExportModel());
-		//RegelsetController regelsetController = new RegelsetController(new RegelsetModel());
-		//ImportController importController = new ImportController(new ImportModel());
+		
 
 		// Start MainView
 		mainController.show();
 
-		// Start ExportView
-		//exportController.show();
-
-		// Start RegelsetView
-		//regelsetController.show();
-
-
-		// Start ImportView
 		// Bitte zum Testen die config.properties im 'resources' Ordner wo das
 		// resourceBundle auswählen...
 		//importController.show();
