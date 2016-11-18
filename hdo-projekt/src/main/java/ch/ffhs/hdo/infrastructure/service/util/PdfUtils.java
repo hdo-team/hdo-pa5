c1ed9c39ad20d61c427fa731cad9bb670a28caa0
denisbittante <denisbittante@gmail.com>
2016-11-18 06:12:34 +0100

    HDO-9: Eigenschaften von PDF's laden
    Eigenschaft können geleasen werden

--- hdo-projekt/src/main/java/ch/ffhs/hdo/infrastructure/service/util/PdfUtils.java
+++ hdo-projekt/src/main/java/ch/ffhs/hdo/infrastructure/service/util/PdfUtils.java
@@ -2,6 +2,7 @@ package ch.ffhs.hdo.infrastructure.service.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,6 +10,7 @@ import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.io.RandomAccessFile;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 public class PdfUtils {
@@ -27,7 +29,7 @@ public class PdfUtils {
 			cosDoc = parser.getDocument();
 			pdfStripper = new PDFTextStripper();
 			pdDoc = new PDDocument(cosDoc);
-		
+
 			String parsedText = pdfStripper.getText(pdDoc);
 			return parsedText;
 		} catch (IOException e) {
@@ -37,4 +39,41 @@ public class PdfUtils {
 
 	}
 
+	public static enum PdfMetaData {
+
+		PAGECOUNT, TITLE, AUTHOR, SUBJECT, KEYWORDS, CREATOR, PRODUCER, CREATION_DATE, MODIFICATION_DATE;
+	}
+
+	public static HashMap<PdfMetaData, Object> getDokumentInformation(File file) {
+
+		PDDocument pdDoc = null;
+		COSDocument cosDoc = null;
+
+		try {
+			PDFParser parser = new PDFParser(new RandomAccessFile(file, "r"));
+			parser.parse();
+			cosDoc = parser.getDocument();
+			pdDoc = new PDDocument(cosDoc);
+
+			PDDocumentInformation info = pdDoc.getDocumentInformation();
+
+			HashMap<PdfMetaData, Object> metaData = new HashMap<PdfMetaData, Object>();
+
+			metaData.put(PdfMetaData.PAGECOUNT, pdDoc.getNumberOfPages());
+			metaData.put(PdfMetaData.TITLE, info.getTitle());
+			metaData.put(PdfMetaData.AUTHOR, info.getAuthor());
+			metaData.put(PdfMetaData.SUBJECT, info.getSubject());
+			metaData.put(PdfMetaData.KEYWORDS, info.getKeywords());
+			metaData.put(PdfMetaData.CREATOR, info.getCreator());
+			metaData.put(PdfMetaData.PRODUCER, info.getProducer());
+			metaData.put(PdfMetaData.CREATION_DATE, info.getCreationDate());
+			metaData.put(PdfMetaData.MODIFICATION_DATE, info.getModificationDate());
+
+			return metaData;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+
+	}
 }
--- hdo-projekt/src/test/java/ch/ffhs/hdo/infrastructure/service/util/PdfUtilsTest.java
+++ hdo-projekt/src/test/java/ch/ffhs/hdo/infrastructure/service/util/PdfUtilsTest.java
@@ -2,9 +2,13 @@ package ch.ffhs.hdo.infrastructure.service.util;
 
 import java.io.File;
 import java.net.URL;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import ch.ffhs.hdo.infrastructure.service.util.PdfUtils.PdfMetaData;
 
 public class PdfUtilsTest {
 
@@ -17,6 +21,28 @@ public class PdfUtilsTest {
 		String readPDF = PdfUtils.readPDF(file);
 
 		Assert.assertTrue(readPDF.length() > 100);
+	}
+
+	@Test
+	public void testReadMetadata() {
+
+		URL url = Thread.currentThread().getContextClassLoader().getResource("TestLesenDokument.pdf");
+		File file = new File(url.getPath());
+
+		HashMap<PdfMetaData, Object> dokumentInformation = PdfUtils.getDokumentInformation(file);
+
+		Assert.assertEquals(new Integer(2), (Integer) dokumentInformation.get(PdfMetaData.PAGECOUNT));
+		Assert.assertEquals("Microsoft® Word 2016", (String) dokumentInformation.get(PdfMetaData.CREATOR));
+		Assert.assertEquals("Denis Bittante", (String) dokumentInformation.get(PdfMetaData.AUTHOR));
+		Assert.assertEquals("TestThema", (String) dokumentInformation.get(PdfMetaData.SUBJECT));
+		Assert.assertEquals("TestTitel", (String) dokumentInformation.get(PdfMetaData.TITLE));
+		Assert.assertEquals("TestTag", (String) dokumentInformation.get(PdfMetaData.KEYWORDS));
+		Assert.assertTrue(((GregorianCalendar) dokumentInformation.get(PdfMetaData.CREATION_DATE))
+				.before(new GregorianCalendar()));
+		Assert.assertTrue(((GregorianCalendar) dokumentInformation.get(PdfMetaData.MODIFICATION_DATE))
+				.before(new GregorianCalendar()));
+		;
+
 	}
 
 }
