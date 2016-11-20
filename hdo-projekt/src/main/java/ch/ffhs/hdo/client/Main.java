6386035e5415ebdaf7629ec0a0f3dadea24bb941
denisbittante <denisbittante@gmail.com>
2016-11-20 17:19:06 +0100

    Executable Jar,
    nach dem builden kann das jar mit : java -jar hdo-0.1.0-SNAPSHOT.jar,
    gestartet werden

--- hdo-projekt/src/main/java/ch/ffhs/hdo/client/Main.java
+++ hdo-projekt/src/main/java/ch/ffhs/hdo/client/Main.java
@@ -1,18 +1,11 @@
 package ch.ffhs.hdo.client;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import ch.ffhs.hdo.client.ui.hauptfenster.MainController;
 import ch.ffhs.hdo.client.ui.hauptfenster.MainModel;
-import ch.ffhs.hdo.infrastructure.ApplicationSettings;
 import ch.ffhs.hdo.infrastructure.service.util.FileHandling;
-import ch.ffhs.hdo.persistence.jdbc.InitDatabase;
 
 public class Main {
 
