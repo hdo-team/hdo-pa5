3f96367703905aab0eabef0db284bda72a2fc66e
denisbittante <denisbittante@gmail.com>
2016-11-23 19:40:01 +0100

    Normalized Model

--- hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/regelset/RegelsetModel.java
+++ hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/regelset/RegelsetModel.java
@@ -7,22 +7,136 @@ import ch.ffhs.hdo.client.ui.utils.IFileModel;
 
 public class RegelsetModel extends Model implements IFileModel {
 
-	private String rulesetName;
-	private String targetDirectory;   // TODO: oder "index" auf Liste; UND Liste
 	private String filenameKonfiguration;
+	private Integer id;
 	private boolean ruleActiv;
-	private List<String> ruleContextList;
 	private List<String> ruleAttributeList;
-	private String rulePropertyFrom;	// TODO: oder besser Object? (kann Date, Long, String,...sein)
-	private String rulePropertyTo;		// TODO:  " " " " incl. getter + setter
-									// TODO: rulePropertyEqual
-									// CompareMode
-	
-	
-	
+	private List<String> ruleContextList;
+	private String rulePropertyFrom; // TODO: oder besser Object? (kann Date,
+										// Long, String,...sein)
+	private String rulePropertyTo; // TODO: " " " " incl. getter + setter
+	private String rulesetName;
+	private String targetDirectory; // TODO: oder "index" auf Liste; UND Liste
+
+	public String getFilenameKonfiguration() {
+
+		return filenameKonfiguration;
+	}
+
+	public String getFilePath() {
+		return getTargetDirectory();
+	}
+
+	final List<String> getRuleAttributeList() {
+
+		return ruleAttributeList;
+
+		// TODO: getRuleContextList().add(xy) funktionier nicht => kein
+		// fireProperty??
+		// TODO: bruacht's das überhaupt oder ist Inhalt "fix" ab DB? kein
+		// dynamisches Nachladen?
+
+		// => final => keine andere LIST zuweisbar? aber doch List.add()
+		// möglich???
+	}
+
+	final List<String> getRuleContextList() {
+
+		return ruleContextList;
+
+		// TODO: getRuleContextList().add(xy) funktionier nicht => kein
+		// fireProperty??
+		// TODO: bruacht's das überhaupt oder ist Inhalt "fix" ab DB? kein
+		// dynamisches Nachladen?
+
+		// => final => keine andere LIST zuweisbar? aber doch List.add()
+		// möglich???
+	}
+
+	public String getRulePropertyFrom() {
+
+		return rulePropertyFrom;
+	}
+
+	public String getRulePropertyTo() {
+
+		return rulePropertyTo;
+	}
+
 	public String getRulesetName() {
-		
+
 		return rulesetName;
+	}
+
+	public String getTargetDirectory() {
+
+		return targetDirectory;
+	}
+
+	public boolean isRuleActiv() {
+
+		return ruleActiv;
+	}
+
+	public void setFilenameKonfiguration(String filenameKonfiguration) {
+
+		String oldValue = this.filenameKonfiguration;
+		this.filenameKonfiguration = filenameKonfiguration;
+		firePropertyChange("filenameKonfiguration", oldValue, filenameKonfiguration);
+	}
+
+	public void setFilePath(String newValue) {
+		setTargetDirectory(newValue);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+
+		Integer oldValue = this.id;
+		this.id = id;
+		firePropertyChange("id", oldValue, id);
+	}
+
+	public void setRuleActiv(boolean ruleActiv) {
+
+		boolean oldValue = this.ruleActiv;
+		this.ruleActiv = ruleActiv;
+		firePropertyChange("ruleActiv", oldValue, ruleActiv);
+	}
+
+	public void setRuleAttributeList(List<String> ruleAttributeList) {
+		List<String> oldValue = this.ruleAttributeList;
+		this.ruleAttributeList = ruleAttributeList;
+		firePropertyChange("ruleAttributeList", oldValue, ruleAttributeList);
+
+		// TODO is firePorperty überhaupt nötig??? kein dynamisches nachladen
+		// von DB
+	}
+
+	public void setRuleContextList(List<String> ruleContextList) {
+		List<String> oldValue = this.ruleContextList;
+		this.ruleContextList = ruleContextList;
+		firePropertyChange("ruleContextList", oldValue, ruleContextList);
+
+		// TODO is firePorperty überhaupt nötig??? kein dynamisches nachladen
+		// von DB
+	}
+
+	public void setRulePropertyFrom(String rulePropertyFrom) {
+
+		String oldValue = this.rulePropertyFrom;
+		this.rulePropertyFrom = rulePropertyFrom;
+		firePropertyChange("rulePropertyFrom", oldValue, rulePropertyFrom);
+	}
+
+	public void setRulePropertyTo(String rulePropertyTo) {
+
+		String oldValue = this.rulePropertyTo;
+		this.rulePropertyTo = rulePropertyTo;
+		firePropertyChange("rulePropertyTo", oldValue, rulePropertyTo);
 	}
 
 	public void setRulesetName(String rulesetName) {
@@ -32,109 +146,10 @@ public class RegelsetModel extends Model implements IFileModel {
 		firePropertyChange("rulesetName", oldValue, rulesetName);
 	}
 
-	public String getTargetDirectory() {
-		
-		return targetDirectory;
-	}
-	
 	public void setTargetDirectory(String targetDirectory) {
-		
+
 		String oldValue = this.targetDirectory;
 		this.targetDirectory = targetDirectory;
 		firePropertyChange("targetDirectory", oldValue, targetDirectory);
-	}
-	
-	public String getFilenameKonfiguration() {
-		
-		return filenameKonfiguration;
-	}
-	
-	public void setFilenameKonfiguration(String filenameKonfiguration) {
-		
-		String oldValue = this.filenameKonfiguration;
-		this.filenameKonfiguration = filenameKonfiguration;
-		firePropertyChange("filenameKonfiguration", oldValue, filenameKonfiguration);
-	}
-	
-	public boolean isRuleActiv() {
-
-		return ruleActiv;
-	}
-
-	public void setRuleActiv(boolean ruleActiv) {
-
-		boolean oldValue = this.ruleActiv;
-		this.ruleActiv = ruleActiv;
-		firePropertyChange("ruleActiv", oldValue, ruleActiv);
-	}
-	
-	final List<String> getRuleContextList() {
-		
-		return ruleContextList;		
-		
-		// TODO: getRuleContextList().add(xy) funktionier nicht => kein fireProperty??
-		//  TODO: bruacht's das überhaupt oder ist Inhalt "fix" ab DB? kein dynamisches Nachladen?
-		
-		
-		// => final => keine andere LIST zuweisbar? aber doch List.add() möglich???
-	}
-	
-	public void setRuleContextList(List<String> ruleContextList) {
-		List<String> oldValue = this.ruleContextList;
-		this.ruleContextList = ruleContextList;
-		firePropertyChange("ruleContextList", oldValue, ruleContextList);
-		
-		// TODO is firePorperty überhaupt nötig??? kein dynamisches nachladen von DB
-	}
-	
-	final List<String> getRuleAttributeList() {
-		
-		return ruleAttributeList;		
-		
-		// TODO: getRuleContextList().add(xy) funktionier nicht => kein fireProperty??
-		//  TODO: bruacht's das überhaupt oder ist Inhalt "fix" ab DB? kein dynamisches Nachladen?
-		
-		
-		// => final => keine andere LIST zuweisbar? aber doch List.add() möglich???
-	}
-	
-	public void setRuleAttributeList(List<String> ruleAttributeList) {
-		List<String> oldValue = this.ruleAttributeList;
-		this.ruleAttributeList = ruleAttributeList;
-		firePropertyChange("ruleAttributeList", oldValue, ruleAttributeList);
-		
-		// TODO is firePorperty überhaupt nötig??? kein dynamisches nachladen von DB
-	}
-	
-	public String getRulePropertyFrom() {
-		
-		return rulePropertyFrom;
-	}
-	
-	public void setRulePropertyFrom(String rulePropertyFrom) {
-		
-		String oldValue = this.rulePropertyFrom;
-		this.rulePropertyFrom = rulePropertyFrom;
-		firePropertyChange("rulePropertyFrom", oldValue, rulePropertyFrom);
-	}
-	
-	public String getRulePropertyTo() {
-		
-		return rulePropertyTo;
-	}
-	
-	public void setRulePropertyTo(String rulePropertyTo) {
-		
-		String oldValue = this.rulePropertyTo;
-		this.rulePropertyTo = rulePropertyTo;
-		firePropertyChange("rulePropertyTo", oldValue, rulePropertyTo);
-	}
-
-	public String getFilePath() {
-		return getTargetDirectory();
-	}
-
-	public void setFilePath(String newValue) {
-		setTargetDirectory(newValue);
 	}
 }
