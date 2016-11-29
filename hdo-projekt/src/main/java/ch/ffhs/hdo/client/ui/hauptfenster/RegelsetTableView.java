254b6a688e2d3240cfbaa79267bf85881c33faba
segjo <jonas_segessemann@hotmail.com>
2016-11-30 00:18:22 +0100

    HDO-88
    Regelsets können ausgewählt und erstellt werden

--- hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/hauptfenster/RegelsetTableView.java
+++ hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/hauptfenster/RegelsetTableView.java
@@ -1,24 +1,24 @@
 package ch.ffhs.hdo.client.ui.hauptfenster;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 import java.util.ResourceBundle;
 
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
 import com.jgoodies.forms.builder.FormBuilder;
 
 import ch.ffhs.hdo.client.ui.base.View;
+import ch.ffhs.hdo.client.ui.regelset.RegelsetModel;
+import ch.ffhs.hdo.client.ui.regelset.executable.RegelsetViewStartOperation;
 
 public class RegelsetTableView extends View<RegelsetTableModel> {
-	
 
 	private final String I18N = "hdo.main";
 	private JPanel jPanel;
@@ -30,15 +30,15 @@ public class RegelsetTableView extends View<RegelsetTableModel> {
 	private JButton editButton;
 	private JButton newButton;
 	private JButton deleteButton;
-	private JLabel stateLabel;
+	private JButton stateButton;
 
 	public RegelsetTableView(ResourceBundle resourceBundle, RegelsetTableModel model) {
 		super(resourceBundle);
 		this.setModel(model);
 		initComponents();
-		
+
 	}
-	
+
 	private void initComponents() {
 		createComponents();
 		layoutForm();
@@ -47,64 +47,95 @@ public class RegelsetTableView extends View<RegelsetTableModel> {
 
 	private void createComponents() {
 		jPanel = new JPanel();
-		
-		getModel().createAbstractTableModel(new String[]{getMessage(I18N + ".rulsetTableHeader.name"),getMessage(I18N + ".rulsetTableHeader.directory"),getMessage(I18N + ".rulsetTableHeader.active")});
+
+		getModel().createAbstractTableModel(new String[] { getMessage(I18N + ".rulsetTableHeader.name"),
+				getMessage(I18N + ".rulsetTableHeader.directory"), getMessage(I18N + ".rulsetTableHeader.active") });
 		regelsetTable = new JTable(getModel().getAbstractModel());
 		tableScrollPane = new JScrollPane(regelsetTable);
-		
-		//Toolbar
+
+		// Toolbar
 		toolbarPanel = new JPanel();
 		prioUpButton = new JButton(getMessage(I18N + ".button.prioUp"));
 		prioDownButton = new JButton(getMessage(I18N + ".button.prioDown"));
 		newButton = new JButton(getMessage(I18N + ".button.newRegelset"));
 		editButton = new JButton(getMessage(I18N + ".button.editRegelset"));
 		deleteButton = new JButton(getMessage(I18N + ".button.deleteRegelset"));
-		stateLabel = new JLabel(getMessage(I18N + ".label.stateStop"));
-		
-		regelsetTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+		stateButton = new JButton(getMessage(I18N + ".button.stateStop"));
 
-		    public void valueChanged(ListSelectionEvent event) {
-		        if (regelsetTable.getSelectedRow() > -1) {
-		            // print first column value from selected row
-		            System.out.println(regelsetTable.getValueAt(regelsetTable.getSelectedRow(), 0).toString());
-		        }
-		    }});
+		prioUpButton.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				//getHandler().performOperationWithArgs(RegelsetPrioOperation.class, true); //TODO Prio Change Action
+			}
+		});	
 		
-		//prioUpButton.addActionListener(new prioUpAction());
+		prioDownButton.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				//getHandler().performOperationWithArgs(RegelsetPrioOperation.class, true); //TODO Prio Change Action
+			}
+		});	
+		
+		newButton.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				getHandler().performOperationWithArgs(RegelsetViewStartOperation.class, new RegelsetModel());
+			}
+		});
+		
+		editButton.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				if (regelsetTable.getSelectedRow() > -1) {
+					getHandler().performOperationWithArgs(RegelsetViewStartOperation.class,
+							getModel().getRulsetList().get(regelsetTable.getSelectedRow()));
+				}
+			}
+		});
+		
+		deleteButton.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				//getHandler().performOperationWithArgs(RegelsetDeleteOperation.class, true); //TODO Delete Action
+			}
+		});
+		
+		stateButton.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				//getHandler().performOperationWithArgs(RunOperation.class, true); //TODO start/stop sort
+			}
+		});
+
 	}
 
 	private void layoutForm() {
 		regelsetTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		regelsetTable.setDragEnabled(true);
 		jPanel.setLayout(new BorderLayout());
-		
-		//Toolbar
+
+		// Toolbar
 		FormBuilder builder = FormBuilder.create()
-				.columns("right:pref, 5dlu,[20dlu, pref], 5dlu,[20dlu, pref], 20dlu,[20dlu, pref], 5dlu,[20dlu, pref], 5dlu,[20dlu, pref], 20dlu,[20dlu, pref]")
+				.columns(
+						"right:pref, 5dlu,[20dlu, pref], 5dlu,[20dlu, pref], 20dlu,[20dlu, pref], 5dlu,[20dlu, pref], 5dlu,[20dlu, pref], 20dlu,[20dlu, pref]")
 				.rows("p, $lg, p");
 		builder.add(prioUpButton).rc(3, 3);
 		builder.add(prioDownButton).rc(3, 5);
 		builder.add(newButton).rc(3, 7);
 		builder.add(editButton).rc(3, 9);
 		builder.add(deleteButton).rc(3, 11);
-		builder.add(stateLabel).rc(3, 13);;
+		builder.add(stateButton).rc(3, 13);
+		;
 		builder.padding(new EmptyBorder(5, 5, 5, 5));
 		toolbarPanel = builder.build();
-		
+
 		jPanel.add(regelsetTable.getTableHeader(), BorderLayout.NORTH);
-		jPanel.add(regelsetTable, BorderLayout.CENTER); //TODO tableScrollPane statt regelsetTable
+		jPanel.add(regelsetTable, BorderLayout.CENTER); // TODO tableScrollPane
+														// statt regelsetTable
 		jPanel.add(toolbarPanel, BorderLayout.SOUTH);
 	}
-	
+
 	@Override
 	public void configureBindings() {
 
-		
 	}
-	
+
 	public JPanel getPanel() {
 		return jPanel;
 	}
-
 
 }
