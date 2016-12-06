2aca043c0cf57f8dc572f9d57a96eb04aa1039ba
denisbittante <denisbittante@gmail.com>
2016-12-06 18:02:30 +0100

    HDO-23 Dokumentmodel nachgebessert

--- hdo-projekt/src/main/java/ch/ffhs/hdo/domain/document/DocumentModel.java
+++ hdo-projekt/src/main/java/ch/ffhs/hdo/domain/document/DocumentModel.java
@@ -10,18 +10,34 @@ import ch.ffhs.hdo.infrastructure.service.util.PdfUtils.PdfMetaData;
 
 public class DocumentModel {
 
-	File file;
-	HashMap<FileMetaData, Object> fileMetadata;
-	HashMap<PdfMetaData, Object> pdfMetadata;
-	String content;
+	private String content;
+	private File file;
+	private HashMap<FileMetaData, Object> fileMetadata;
+	private HashMap<PdfMetaData, Object> pdfMetadata;
 
 	public DocumentModel(File file) {
 
 		this.file = file;
-		fileMetadata = FileHandling.getFileMetaData(file);
-		pdfMetadata = PdfUtils.getDocumentInformation(file);
+		this.fileMetadata = FileHandling.getFileMetaData(file);
+		this.pdfMetadata = PdfUtils.getDocumentInformation(file);
 		this.content = PdfUtils.readPDF(file);
 
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public HashMap<FileMetaData, Object> getFileMetadata() {
+		return fileMetadata;
+	}
+
+	public HashMap<PdfMetaData, Object> getPdfMetadata() {
+		return pdfMetadata;
 	}
 
 }
