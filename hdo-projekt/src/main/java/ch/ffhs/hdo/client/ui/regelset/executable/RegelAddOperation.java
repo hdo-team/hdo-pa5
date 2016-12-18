2443afda08e4de622f744b9fc35d79293a2b266e
Daniel Crazzolara <crazzolarad@gmail.ch>
2016-12-18 18:30:23 +0100

    nicht verwendete Klassen gelöscht

--- hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/regelset/executable/RegelAddOperation.java
+++
@@ -1,7 +0,0 @@
-package ch.ffhs.hdo.client.ui.regelset.executable;
-
-import ch.ffhs.hdo.client.ui.base.viewhandler.ViewOperation;
-
-public interface RegelAddOperation extends ViewOperation {
-
-}
--- hdo-projekt/src/main/java/ch/ffhs/hdo/client/ui/regelset/executable/RegelDeleteOperation.java
+++
@@ -1,7 +0,0 @@
-package ch.ffhs.hdo.client.ui.regelset.executable;
-
-import ch.ffhs.hdo.client.ui.base.viewhandler.ViewOperation;
-
-public interface RegelDeleteOperation extends ViewOperation {
-
-}
